e27ef03c787552faddc5f0eed3e97fccf8cc8d46
gaoxiang.huang <hgx1233626@163.com>
2023-04-23 15:12:52 +0800

    feat: note

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const Note = ({note}) => {
+    return (
+        <li>{note.content}</li>
+    )
+}
+const App = (props) => {
+    const {notes} = props
+    return (
+        <div>
+            <h1>Notes</h1>
+            <ul>
+                {notes.map(note =>
+                    <Note key={note.id} note={note}/>
+                )}
+            </ul>
+        </div>
+    );
 }
 
 export default App;
