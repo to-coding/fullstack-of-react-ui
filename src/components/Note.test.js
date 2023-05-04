9c5b2245d53ad103026422da4e3d7c363ac0bbff
gaoxiang.huang <hgx1233626@163.com>
2023-05-04 11:04:59 +0800

    add test

--- src/components/Note.test.js
+++ src/components/Note.test.js
@@ -1,5 +1,6 @@
 import { render, screen } from '@testing-library/react'
 import Note from './Note'
+import userEvent from '@testing-library/user-event'
 
 test('renders content', () => {
     const note = {
@@ -8,7 +9,26 @@ test('renders content', () => {
     }
 
     render(<Note note={note}/>)
+    // screen.debug()
 
     const element = screen.getByText('Component testing is done with react-testing-library')
     expect(element).toBeDefined()
+})
+
+test('clicking the button calls event handler once', async () => {
+    const note = {
+        content: 'Component testing is done with react-testing-library',
+        important: true
+    }
+
+    const mockHandler = jest.fn()
+    render(
+        <Note note={note} toggleImportance={mockHandler()}/>
+    )
+
+    const user = userEvent.setup()
+    const button = screen.getByText('make not important')
+    await user.click(button)
+
+    expect(mockHandler.mock.calls).toHaveLength(1)
 })
